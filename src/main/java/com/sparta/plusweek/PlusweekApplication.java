2bd2190e5eac46a7ff9fe416a23685ce3ef39345
peter202597 <peter202597@gmail.com>
2023-12-13 16:46:11 +0900

    first commit

---
+++ src/main/java/com/sparta/plusweek/PlusweekApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.plusweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlusweekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlusweekApplication.class, args);
+    }
+
+}
